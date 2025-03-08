0f2f5da6ee7ba774ee1465fe1cfc890c2505d972
Jeam547 <muhammadjaemizha2000@gmail.com>
2025-03-08 09:23:44 +0700

    commit terbaik

---
+++ login.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+include 'config.php';
+
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM users WHERE username = '$username'";
+    $result = mysqli_query($conn, $query);
+    $user = mysqli_fetch_assoc($result);
+
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['username'] = $username;
+        header("Location: index.php");
+        exit();
+    } else {
+        echo "Username atau password salah!";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form method="POST">
+        <label>Username:</label>
+        <input type="text" name="username" required><br>
+        <label>Password:</label>
+        <input type="password" name="password" required><br>
+        <button type="submit" name="login">Login</button>
+    </form>
+    <p>Belum punya akun? <a href="register.php">Register</a></p>
+</body>
+</html>
